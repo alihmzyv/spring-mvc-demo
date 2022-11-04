4d7bb6583e7db9768507b79a6a33c44e64ba39d7
alihmzyv <alihmzyv@gmail.com>
2022-11-04 18:19:11 +0400

    last studies

--- src/main/java/com/luv2code/springdemo/mvc/HomeController.java
+++ src/main/java/com/luv2code/springdemo/mvc/HomeController.java
@@ -1,13 +1,13 @@
 package com.luv2code.springdemo.mvc;
 
-import com.luv2code.springdemo.mvc.entity.Student;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
+@Controller //mvc controller
 public class HomeController {
     @RequestMapping("/")
-    public String showHomePage() {
-        return "main-page";
+    public String showMyPage() {
+        return "main-menu.jsp";
     }
 }
--- src/main/java/com/luv2code/springdemo/mvc/SpringMvcDemoApplication.java
+++ src/main/java/com/luv2code/springdemo/mvc/SpringMvcDemoApplication.java
@@ -1,14 +1,13 @@
 package com.luv2code.springdemo.mvc;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class SpringMvcDemoApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvcDemoApplication.class, args);
 	}
+
 }
